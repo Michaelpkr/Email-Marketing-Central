fda018bb258f172116554d811001b8ce12e50762
Michaelpkr <92385361+Michaelpkr@users.noreply.github.com>
2023-08-03 14:20:22 -0500

    Commit

--- src/Components/Output.js
+++ src/Components/Output.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 
 export default function Output(props) {
   const { formInput, currentHTML } = props;
-  const api_key = "pk_c5e29e3c4ab9aa01185a8c05f84bc219ab";
 
   const renderHTML = Object.entries(formInput).reduce(
     (html, [key, value]) => {
@@ -16,17 +15,13 @@ export default function Output(props) {
     const textarea = document.createElement('textarea');
     textarea.value = renderHTML;
 
-    // Append the textarea to the document
     document.body.appendChild(textarea);
 
-    // Select the text within the textarea
     textarea.select();
     textarea.setSelectionRange(0, 99999); // For mobile devices
 
-    // Copy the selected text to the clipboard
     document.execCommand('copy');
 
-    // Remove the temporary textarea
     document.body.removeChild(textarea);
   };
 
